d1100187d467d8fe9957aa5c5c3e160af839dae0
Raphael Sant'Anna <raphael.santanna@visagio.com>
2017-10-21 18:43:26 -0200

    First commit with nucleotide count example

---
+++ src/test/java/br/dcc/ufrj/rsantanna/genuvem/examples/NucleotideCountTest.java
@@ -0,0 +1,23 @@
+package br.dcc.ufrj.rsantanna.genuvem.examples;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Test;
+
+import br.dcc.ufrj.rsantanna.genuvem.examples.NucleotideCount.NucleotideMapper;
+
+public class NucleotideCountTest {
+
+	@Test
+	public void test() throws IOException {
+		Text value = new Text("");
+
+		new MapDriver<Object, Text, Text, IntWritable>()
+			.withMapper(new NucleotideMapper())
+			.withInput("Teste", value)
+			.runTest();
+	}
+}
